5fa79ebfec345120fdbf50a772c23a574a6e38a2
Lucy Liu <lucy.liu94@gmail.com>
2021-11-15 15:52:48 -0500

    styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,14 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithubAlt } from '@fortawesome/free-brands-svg-icons'
+
 const Footer = props => {
   
   return (
     <div className="Footer">
         <footer>
-          <p>Created by: Jameson, Lucy and Nathan | &copy; 2021</p>
+          <p>Created by: Jameson 
+            <a href="https://github.com/code-weather"><FontAwesomeIcon icon={faGithubAlt} pulse/></a> 
+             , Lucy <FontAwesomeIcon icon={faGithubAlt} pulse /> and Nathan <FontAwesomeIcon icon={faGithubAlt} pulse /> | &copy; 2021</p>
         </footer>   
     </div>
   )
