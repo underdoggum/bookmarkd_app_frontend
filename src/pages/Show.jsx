8c2b919aceca605d4d9d1cc84954405e62bf5980
Jameson Wang <jamesonw10@gmail.com>
2021-11-15 12:18:50 -0500

    frontend update

--- src/pages/Show.jsx
+++ src/pages/Show.jsx
@@ -1,7 +1,6 @@
 import { useParams, useNavigate} from "react-router-dom";
 import { useEffect, useState } from "react";
 
-
 const Show = props => {
   const navigate = useNavigate()
   const params = useParams();
